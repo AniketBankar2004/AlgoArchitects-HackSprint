1cca82544ff861c07bdd26d56e6186d07e262c0c
AniketBankar2004 <143994113+AniketBankar2004@users.noreply.github.com>
2025-04-11 12:00:02 +0530

    added skillcheck

--- evaluation/app.py
+++ evaluation/app.py
@@ -45,27 +45,44 @@ def clean_text(text):
 
 
 
-def evaluate(description: str, resumes: dict, model) -> dict:
+def evaluate(description: str, resumes: dict, model,skills: list) -> dict:
     description_vector = model.encode(description, convert_to_tensor=True)
-    scoring = {}
+    results = {}
 
     for name, resume in resumes.items():
         res_vector = model.encode(resume, convert_to_tensor=True)
         sim_score = util.pytorch_cos_sim(description_vector, res_vector)[0][0].item()
-        scoring[name] = round(sim_score, 4)
+        
+        skill_present = {
+            skill : (skill.lower() in resume.lower() )
+            for skill in skills
+        }
+
+        results[name] = {
+            "score": round(sim_score, 4),
+            "skills": skill_present
+        }
 
     
-    sorted_scores = dict(sorted(scoring.items(), key=lambda item: item[1]))
+    sorted_results = dict(sorted(results.items(), key=lambda item: item[1]['score']))
+    return sorted_results
 
-    return sorted_scores
+
 @app.route('/scan', methods=['POST'])
 def upload_files():
 
     name_job_description = request.form.get('job_description')
+    raw_skills = request.form.get('skills')
+
+    if not raw_skills:
+        return jsonify({"error": "Missing 'skills' in form data"}), 400
+
     if not name_job_description:
         return jsonify({"error": "Missing 'name_job_description' in form data"}), 400
     
     cleaned_description = clean_text(name_job_description)
+    skills = [s.strip() for s in raw_skills.split(',') if s.strip()]
+    skills = [skill.lower() for skill in skills]
 
     if 'files' not in request.files:
         return jsonify({"error": "No files part in the request"}), 400
@@ -83,9 +100,9 @@ def upload_files():
             except Exception as e:
                 extracted[filename] = f"Error extracting text: {str(e)}"
 
-    score = evaluate(cleaned_description,extracted,model)
+    result = evaluate(cleaned_description,extracted,model,skills)
 
-    return jsonify(score)
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
